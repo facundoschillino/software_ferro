21995ad1706aba96604eba0bdcdb93f8b31befd1
Facundo Gabriel Schillino Gariglio <111167970+facundoschillino@users.noreply.github.com>
2025-08-09 19:47:24 -0300

    hola si

---
+++ build.sh
@@ -0,0 +1,8 @@
+set -o errexit
+
+pip install -r ./requirements.txt
+
+cd $(dirname $(find . | grep manage.py$))
+python manage.py collectstatic --no-input
+python manage.py migrate
+python manage.py createsuperuser --username admin --email "facundoschillino01@gmail.com" --noinput || true
---
+++ run.sh
@@ -0,0 +1,4 @@
+set -o errexit
+
+cd $(dirname $(find . | grep manage.py$))
+gunicorn $(dirname $(find . | grep wsgi.py$) | sed "s/\.\///g").wsgi:application
