0ff82a3a8691afcf7335a7448cd98544ac6a4a06
Facundo Gabriel Schillino Gariglio <111167970+facundoschillino@users.noreply.github.com>
2025-08-10 20:07:31 -0300

    run.sh cambiado

--- run.sh
+++ run.sh
@@ -1,18 +1,5 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
+# exit on error
+set -o errexit
 
-# 1) Ir a la carpeta del manage.py
-if [ -f "./manage.py" ]; then
-  ROOT="."
-else
-  ROOT="$(dirname "$(find . -maxdepth 3 -type f -name manage.py -not -path "*/.venv/*" | head -n1)")"
-  cd "$ROOT"
-fi
-
-# 2) Tareas previas
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-
-# 3) Leer WSGI_APPLICATION desde settings y lanzar Gunicorn
-WSGI_APP="$(python manage.py shell -c 'from django.conf import settings; print(settings.WSGI_APPLICATION)')"
-exec gunicorn "$WSGI_APP" --bind "0.0.0.0:${PORT:-8000}"
+cd $(dirname $(find . | grep manage.py$))
+gunicorn $(dirname $(find . | grep wsgi.py$) | sed "s/\.\///g").wsgi:application
